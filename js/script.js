e5daa0abf3deb8eb9e45a2071f158243f4794bc1
IJ iMac <mac-mtechcork@MacMTecorksiMac.station>
2025-03-03 21:12:58 +0000

    add link to address and phone number

--- js/script.js
+++ js/script.js
@@ -1,31 +1,5 @@
-$(".slider_container").slick({
-  dots: true,
-  centerMode: true,
-  centerPadding: "60px",
-  slidesToShow: 3,
-  autoplay: true,
-  speed: 3000,
-  responsive: [
-    {
-      breakpoint: 768,
-      settings: {
-        arrows: false,
-        centerMode: true,
-        centerPadding: "40px",
-        slidesToShow: 3,
-      },
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        arrows: false,
-        centerMode: true,
-        centerPadding: "40px",
-        slidesToShow: 1,
-      },
-    },
-  ],
-});
+
+
 
 // increase image size onclick
 var fullImgBox = document.getElementById("fullImgBox");
